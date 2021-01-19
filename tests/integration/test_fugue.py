0e079ecc6ed612ccd3745bdb5bb9851a04c6e8e8
Nils Braun <nils-braun@users.noreply.github.com>
2021-01-19 22:02:22 +0100

    Adding fugue integration and tests (#116)
    * Adding fugue integration and tests
    
    * Stylefix

---
+++ tests/integration/test_fugue.py
@@ -0,0 +1,22 @@
+import pytest
+
+import pandas as pd
+from pandas.testing import assert_frame_equal
+
+fugue_sql = pytest.importorskip("fugue_sql")
+# needs to be imported after the check for fugue
+from dask_sql.integrations.fugue import DaskSQLExecutionEngine
+
+
+def test_simple_statement():
+    with fugue_sql.FugueSQLWorkflow(DaskSQLExecutionEngine) as dag:
+        # Define a data frame and use it
+        df = dag.df([[0, "hello"], [1, "world"]], "a:int64,b:str")
+        dag(
+            """
+        result = SELECT * FROM df WHERE a > 0
+        """
+        )
+
+        return_df = dag["result"].compute().as_pandas()
+        assert_frame_equal(return_df, pd.DataFrame({"a": [1], "b": ["world"]}))
