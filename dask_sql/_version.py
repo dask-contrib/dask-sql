f380a226e31be6923a605df885bba4035c60b678
Nils Braun <nils-braun@users.noreply.github.com>
2021-01-30 21:45:30 +0100

    Importlib import (#125)
    * Fixed the import of importlib for older python versions
    
    * Added a test for different python versions
    
    * Fix dependency conflicts while installing tests
    
    * Exclude python 3.9, as numba (needed by dask-ml) can not be installed

--- dask_sql/_version.py
+++ dask_sql/_version.py
@@ -1,8 +1,11 @@
-from importlib.metadata import version, PackageNotFoundError
+try:
+    from importlib import metadata
+except ImportError:  # for Python < 3.8
+    import importlib_metadata as metadata
 
 
 def get_version():
     try:
-        return version("dask-sql")
-    except PackageNotFoundError:  # pragma: no cover
+        return metadata.version("dask-sql")
+    except metadata.PackageNotFoundError:  # pragma: no cover
         pass
