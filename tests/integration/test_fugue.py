20518e2984e99421ce71b278d2331fd0c3bb8f1c
Han Wang <goodwanghan@gmail.com>
2021-04-27 10:16:01 -0700

    upgrade fugue dependency (#160)
    * upgrade fugue dependency
    
    * update
    
    * update

--- tests/integration/test_fugue.py
+++ tests/integration/test_fugue.py
@@ -8,21 +8,33 @@ import dask.dataframe as dd
 from dask_sql import Context
 
 # needs to be imported after the check for fugue
-from dask_sql.integrations.fugue import DaskSQLExecutionEngine, fsql
+from dask_sql.integrations.fugue import DaskSQLExecutionEngine, fsql_dask
 
 
 def test_simple_statement():
-    with fugue_sql.FugueSQLWorkflow(DaskSQLExecutionEngine) as dag:
-        # Define a data frame and use it
-        df = dag.df([[0, "hello"], [1, "world"]], "a:int64,b:str")
-        dag(
-            """
-        result = SELECT * FROM df WHERE a > 0
+    dag = fugue_sql.FugueSQLWorkflow()
+    df = dag.df([[0, "hello"], [1, "world"]], "a:int64,b:str")
+    dag(
         """
-        )
+    SELECT * FROM df WHERE a > 0 YIELD DATAFRAME AS result
+    """
+    )
+    result = dag.run(DaskSQLExecutionEngine)
 
-        return_df = dag["result"].compute().as_pandas()
-        assert_frame_equal(return_df, pd.DataFrame({"a": [1], "b": ["world"]}))
+    return_df = result["result"].as_pandas()
+    assert_frame_equal(return_df, pd.DataFrame({"a": [1], "b": ["world"]}))
+
+    # A more elegant way to do things
+    pdf = pd.DataFrame([[0, "hello"], [1, "world"]], columns=["a", "b"])
+    result = fugue_sql.fsql(
+        """
+    SELECT * FROM df WHERE a > 0 YIELD DATAFRAME AS result
+    """,
+        df=pdf,
+    ).run("dask")
+
+    return_df = result["result"].as_pandas()
+    assert_frame_equal(return_df, pd.DataFrame({"a": [1], "b": ["world"]}))
 
 
 def test_fsql():
@@ -30,7 +42,7 @@ def test_fsql():
         assert_frame_equal(df, pd.DataFrame({"a": [1]}))
 
     # the simplest case: the SQL does not use any input and does not generate output
-    fsql(
+    fsql_dask(
         """
     CREATE [[0],[1]] SCHEMA a:long
     SELECT * WHERE a>0
@@ -44,7 +56,7 @@ def test_fsql():
         "df", dd.from_pandas(pd.DataFrame([[0], [1]], columns=["a"]), npartitions=2)
     )
 
-    fsql(
+    fsql_dask(
         """
     SELECT * FROM df WHERE a>0
     OUTPUT USING assert_eq
@@ -54,7 +66,7 @@ def test_fsql():
 
     # for dataframes with name, they can register back to the Context (register=True)
     # the return of fsql is the dict of all dask dataframes with explicit names
-    result = fsql(
+    result = fsql_dask(
         """
     x=SELECT * FROM df WHERE a>0
     OUTPUT USING assert_eq
@@ -84,7 +96,7 @@ def test_fsql():
     def cumsum(df: pd.DataFrame) -> pd.DataFrame:
         return df.cumsum()
 
-    fsql(
+    fsql_dask(
         """
     data = SELECT * FROM df1 WHERE a>0 UNION ALL SELECT * FROM df2 WHERE a>0 PERSIST
     result1 = TRANSFORM data PREPARTITION BY a PRESORT b USING cumsum
