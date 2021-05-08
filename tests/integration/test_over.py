44883875e6ac31647e4a885afef15aacf375134e
Nils Braun <nilslennartbraun@gmail.com>
2021-05-08 16:14:47 +0200

    Finally, also implement OVER in the new logic

--- tests/integration/test_over.py
+++ tests/integration/test_over.py
@@ -10,13 +10,14 @@ def test_over_with_sorting(c, user_table_1):
         user_id,
         ROW_NUMBER() OVER (ORDER BY user_id, b) AS R
     FROM user_table_1
+    ORDER BY user_id, b
     """
     )
     df = df.compute()
 
-    expected_df = pd.DataFrame({"user_id": user_table_1.user_id, "R": [3, 1, 2, 4]})
+    expected_df = pd.DataFrame({"user_id": [1, 2, 2, 3], "R": [1, 2, 3, 4]})
     expected_df["R"] = expected_df["R"].astype("Int64")
-    assert_frame_equal(df, expected_df)
+    assert_frame_equal(df.reset_index(drop=True), expected_df.reset_index(drop=True))
 
 
 def test_over_with_partitioning(c, user_table_2):
@@ -26,13 +27,14 @@ def test_over_with_partitioning(c, user_table_2):
         user_id,
         ROW_NUMBER() OVER (PARTITION BY c) AS R
     FROM user_table_2
+    ORDER BY user_id, c
     """
     )
     df = df.compute()
 
-    expected_df = pd.DataFrame({"user_id": user_table_2.user_id, "R": [1, 1, 1, 1]})
+    expected_df = pd.DataFrame({"user_id": [1, 1, 2, 4], "R": [1, 1, 1, 1]})
     expected_df["R"] = expected_df["R"].astype("Int64")
-    assert_frame_equal(df, expected_df)
+    assert_frame_equal(df.reset_index(drop=True), expected_df.reset_index(drop=True))
 
 
 def test_over_with_grouping_and_sort(c, user_table_1):
@@ -42,13 +44,14 @@ def test_over_with_grouping_and_sort(c, user_table_1):
         user_id,
         ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY b) AS R
     FROM user_table_1
+    ORDER BY user_id, b
     """
     )
     df = df.compute()
 
-    expected_df = pd.DataFrame({"user_id": user_table_1.user_id, "R": [2, 1, 1, 1]})
+    expected_df = pd.DataFrame({"user_id": [1, 2, 2, 3], "R": [1, 1, 2, 1]})
     expected_df["R"] = expected_df["R"].astype("Int64")
-    assert_frame_equal(df, expected_df)
+    assert_frame_equal(df.reset_index(drop=True), expected_df.reset_index(drop=True))
 
 
 def test_over_with_different(c, user_table_1):
@@ -59,16 +62,17 @@ def test_over_with_different(c, user_table_1):
         ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY b) AS R1,
         ROW_NUMBER() OVER (ORDER BY user_id, b) AS R2
     FROM user_table_1
+    ORDER BY user_id, b
     """
     )
     df = df.compute()
 
     expected_df = pd.DataFrame(
-        {"user_id": user_table_1.user_id, "R1": [2, 1, 1, 1], "R2": [3, 1, 2, 4],}
+        {"user_id": [1, 2, 2, 3], "R1": [1, 1, 2, 1], "R2": [1, 2, 3, 4],}
     )
     for col in ["R1", "R2"]:
         expected_df[col] = expected_df[col].astype("Int64")
-    assert_frame_equal(df, expected_df)
+    assert_frame_equal(df.reset_index(drop=True), expected_df.reset_index(drop=True))
 
 
 def test_over_calls(c):
@@ -86,22 +90,23 @@ def test_over_calls(c):
         MAX(b) OVER (PARTITION BY user_id ORDER BY b) AS O8,
         MIN(b) OVER (PARTITION BY user_id ORDER BY b) AS O9
     FROM user_table_1
+    ORDER BY user_id, b
     """
     )
     df = df.compute()
 
     expected_df = pd.DataFrame(
         {
-            "O1": [2, 1, 1, 1],
-            "O2": [19, 7, 19, 27],
-            "O3": [19, 7, 19, 27],
-            "O4": [17, 7, 17, 27],
-            "O5": [4, 1, 4, 3],
-            "O6": [2, 1, 2, 3],
-            "O7": [2, 1, 2, 1],
-            "O7b": [2, 1, 2, 1],
+            "O1": [1, 1, 2, 1],
+            "O2": [7, 19, 19, 27],
+            "O3": [7, 19, 19, 27],
+            "O4": [7, 17, 17, 27],
+            "O5": [1, 4, 4, 3],
+            "O6": [1, 2, 2, 3],
+            "O7": [1, 2, 2, 1],
+            "O7b": [1, 2, 2, 1],
             "O8": [3, 3, 3, 3],
-            "O9": [1, 3, 1, 3],
+            "O9": [3, 1, 1, 3],
         }
     )
     for col in expected_df.columns:
@@ -109,4 +114,4 @@ def test_over_calls(c):
             continue
         expected_df[col] = expected_df[col].astype("Int64")
     expected_df["O6"] = expected_df["O6"].astype("float64")
-    assert_frame_equal(df, expected_df)
+    assert_frame_equal(df.reset_index(drop=True), expected_df.reset_index(drop=True))
