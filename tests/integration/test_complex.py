e0651619efb988309c744a60ad1c8eff83a967da
Nils Braun <nilslennartbraun@gmail.com>
2021-05-15 22:15:22 +0200

    Skip the complex test on external scheduler, as it causes a flaky test on GitHub

--- tests/integration/test_complex.py
+++ tests/integration/test_complex.py
@@ -1,6 +1,9 @@
 from dask.datasets import timeseries
 
+from tests.integration.fixtures import skip_if_external_scheduler
 
+
+@skip_if_external_scheduler
 def test_complex_query(c):
     df = timeseries(freq="1d").persist()
     c.create_table("timeseries", df)
