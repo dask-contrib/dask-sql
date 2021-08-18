9e74d2306c2de4e77831d1b9644e97e408a72017
Ayush Dattagupta <ayushdg95@gmail.com>
2021-08-18 16:50:42 -0700

    Update PandasInputPlugin to accept cudf dataframes

--- dask_sql/input_utils/pandas.py
+++ dask_sql/input_utils/pandas.py
@@ -1,6 +1,11 @@
 import dask.dataframe as dd
 import pandas as pd
 
+try:
+    import cudf
+except ImportError:
+    cudf = None
+
 from dask_sql.input_utils.base import BaseInputPlugin
 
 
@@ -10,7 +15,8 @@ class PandasInputPlugin(BaseInputPlugin):
     def is_correct_input(
         self, input_item, table_name: str, format: str = None, **kwargs
     ):
-        return isinstance(input_item, pd.DataFrame) or format == "dask"
+        is_cudf_type = cudf and isinstance(input_item, cudf.DataFrame)
+        return is_cudf_type or isinstance(input_item, pd.DataFrame) or format == "dask"
 
     def to_dc(self, input_item, table_name: str, format: str = None, **kwargs):
         npartitions = kwargs.pop("npartitions", 1)
