e94fe7c1bf12ef1d2cb23ad612a8b58cc70a376b
rajagurnath <gurunathrajagopal@gmail.com>
2021-08-29 19:41:32 +0530

    improve dask-sql-cli: 1. Autocompletion 2. AutoSuggestion 3. Added progressBar 4. Added metacommands

--- dask_sql/cmd.py
+++ dask_sql/cmd.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 import traceback
 from argparse import ArgumentParser
 from functools import partial
@@ -6,6 +7,10 @@ from functools import partial
 import pandas as pd
 from dask.datasets import timeseries
 from dask.distributed import Client
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.shortcuts import ProgressBar
 from pygments.lexers.sql import SqlLexer
 
 try:
@@ -16,6 +21,10 @@ except ImportError:  # pragma: no cover
     from prompt_toolkit.layout.lexers import PygmentsLexer
 
 from dask_sql.context import Context
+
+meta_command_completer = WordCompleter(
+    ["\l", "\d?", "\dt", "\df", "\de", "\dm", "\conninfo", "quit"]
+)
 
 
 class CompatiblePromptSession:
@@ -32,17 +41,80 @@ class CompatiblePromptSession:
     """
 
     def __init__(self, lexer) -> None:  # pragma: no cover
+        kwargs = {
+            "lexer": lexer,
+            "history": FileHistory("dask-sql-history"),
+            "auto_suggest": AutoSuggestFromHistory(),
+            "completer": meta_command_completer,
+        }
         try:
             # Version >= 2.0.1: we can use the session object
             from prompt_toolkit import PromptSession
 
-            session = PromptSession(lexer=lexer)
+            session = PromptSession(**kwargs)
             self.prompt = session.prompt
         except ImportError:
             # Version < 2.0: there is no session object
             from prompt_toolkit.shortcuts import prompt
 
-            self.prompt = partial(prompt, lexer=lexer)
+            self.prompt = partial(prompt, **kwargs)
+
+
+def display_markdown(content, **kwargs):
+    df = pd.DataFrame(content, **kwargs)
+    print(df.to_markdown(tablefmt="fancy_grid"))
+    return True
+
+
+def parse_meta_command(sql):
+    command, _, arg = sql.partition(" ")
+    command = command.strip().replace("+", "")
+    return command, arg.strip()
+
+
+def meta_commands(sql: str, context: Context, client: Client) -> bool:
+    """
+     parses metacommands and prints their result
+     returns True if meta commands detected
+    """
+    cmd, schema_name = parse_meta_command(sql)
+    if schema_name == "":
+        schema_name = context.schema_name
+    if cmd == "\d?":
+        available_commands = [
+            ["\l", "List Schemas"],
+            ["\d?]", "Show Commands"],
+            ["\conninfo", "Show Dask Cluster info"],
+            ["\dt [schema]", "List tables"],
+            ["\df [schema]", "List functions"],
+            ["\dm [schema]", "List models"],
+            ["\de [schema]", "List experiments"],
+            ["quit", "Quits dask-sql-cli"],
+        ]
+        return display_markdown(available_commands, columns=["Commands", "Describtion"])
+    elif cmd == "\l":
+        return display_markdown(context.schema.keys())
+    elif cmd == "\dt":
+        return display_markdown(context.schema[schema_name].tables.keys())
+    elif cmd == "\df":
+        return display_markdown(context.schema[schema_name].functions.keys())
+    elif cmd == "\de":
+        return display_markdown(context.schema[schema_name].experiments.keys())
+    elif cmd == "\dm":
+        return display_markdown(context.schema[schema_name].models.keys())
+    elif cmd == "\conninfo":
+        print("Dask cluster info")
+        cluster_info = [
+            ["Dask scheduler", client.scheduler.__dict__["addr"]],
+            ["Dask Dashboard", client.dashboard_link],
+            ["Cluster status", client.status],
+            ["Dask Workers", len(client.cluster.workers)],
+        ]
+        return display_markdown(cluster_info, columns=["components", "value"])
+    elif cmd == "quit":
+        print("Quiting dask-sql ...")
+        sys.exit()
+    return False
 
 
 def cmd_loop(
@@ -94,7 +166,7 @@ def cmd_loop(
         try:
             text = session.prompt("(dask-sql) > ")
         except KeyboardInterrupt:
-            continue
+            sys.exit()
         except EOFError:
             break
 
@@ -103,11 +175,23 @@ def cmd_loop(
         if not text:
             continue
 
-        try:
-            df = context.sql(text, return_futures=False)
-            print(df)
-        except Exception:
-            traceback.print_exc()
+        meta_command_detected = meta_commands(text, context=context, client=client)
+
+        def sqlCaller(text):
+            """
+            This function is for showing progress bar
+            """
+            yield context.sql(text, return_futures=False)
+
+        if not meta_command_detected:
+            try:
+                with ProgressBar() as pb:
+                    for df in pb(sqlCaller(text), total=1, label="Executing"):
+                        if df is not None:  # some sql commands returns None
+                            print(df.to_markdown(tablefmt="fancy_grid"))
+
+            except Exception:
+                traceback.print_exc()
 
 
 def main():  # pragma: no cover
