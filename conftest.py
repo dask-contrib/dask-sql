98e25c80495f5f032f74e811fceb41af909c37fb
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2021-09-10 12:21:43 -0700

    Change --gpu option to --rungpu

--- conftest.py
+++ conftest.py
@@ -4,9 +4,9 @@ pytest_plugins = ["distributed.utils_test", "tests.integration.fixtures"]
 
 
 def pytest_addoption(parser):
-    parser.addoption("--gpu", action="store_true", help="run tests meant for GPU")
+    parser.addoption("--rungpu", action="store_true", help="run tests meant for GPU")
 
 
 def pytest_runtest_setup(item):
-    if "gpu" in item.keywords and not item.config.getoption("--gpu"):
-        pytest.skip("need --gpu option to run")
+    if "gpu" in item.keywords and not item.config.getoption("--rungpu"):
+        pytest.skip("need --rungpu option to run")
