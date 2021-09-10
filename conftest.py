34468e601f35d8d7ed8c33fe5e5979cddd1ada2f
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2021-09-10 12:18:20 -0700

    Add dask-cudf tests with GPU marker

--- conftest.py
+++ conftest.py
@@ -1,3 +1,12 @@
 import pytest
 
 pytest_plugins = ["distributed.utils_test", "tests.integration.fixtures"]
+
+
+def pytest_addoption(parser):
+    parser.addoption("--gpu", action="store_true", help="run tests meant for GPU")
+
+
+def pytest_runtest_setup(item):
+    if "gpu" in item.keywords and not item.config.getoption("--gpu"):
+        pytest.skip("need --gpu option to run")
