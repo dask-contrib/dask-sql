4b61401d8d6dd06bff65c4064739ed072eb3a9e6
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2021-10-06 10:31:18 -0700

    Use assertion to print out warning messages

--- tests/unit/test_context.py
+++ tests/unit/test_context.py
@@ -48,8 +48,7 @@ def test_deprecation_warning(gpu):
 
         c.register_dask_table(data_frame, "table")
 
-        for warning in w:
-            print(warning.message)
+        assert [warning.message for warning in w] == []
 
         assert len(w) == 1
         assert issubclass(w[-1].category, DeprecationWarning)
