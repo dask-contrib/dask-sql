f785d79a9b8d42c5ae5458304b49799b8e5a7d0b
Jeremy Dyer <jdye64@gmail.com>
2021-11-03 19:19:07 +0000

    updated tests per review

--- tests/integration/test_distributeby.py
+++ tests/integration/test_distributeby.py
@@ -1,20 +1,24 @@
 import dask.dataframe as dd
 import pandas as pd
+import pytest
+
+try:
+    import cudf
+    import dask_cudf
+except ImportError:
+    cudf = None
+    dask_cudf = None
 
 
-def test_distribute_by(c):
-    df_0 = pd.DataFrame({"id": [0, 1, 2], "val": [0, 1, 2]})
+@pytest.mark.parametrize("gpu", [False, pytest.param(True, marks=pytest.mark.gpu)])
+def test_distribute_by(c, gpu):
+    df = pd.DataFrame({"id": [0, 1, 2, 1, 2, 3], "val": [0, 1, 2, 1, 2, 3]})
 
-    df_1 = pd.DataFrame({"id": [1, 2, 3], "val": [1, 2, 3]})
-
-    ddf_0 = dd.from_pandas(df_0, npartitions=1)
-    ddf_1 = dd.from_pandas(df_1, npartitions=1)
-    ddf = dd.concat([ddf_0, ddf_1])
-
-    partitioned_ddf = ddf.shuffle(on=["id"])
-    part_0_ids = partitioned_ddf.get_partition(0).compute().id.unique()
-    part_1_ids = partitioned_ddf.get_partition(1).compute().id.unique()
-    assert bool(set(part_0_ids) & set(part_1_ids)) is False
+    if gpu:
+        gdf = cudf.from_pandas(df)
+        ddf = dask_cudf.from_cudf(df, npartitions=2)
+    else:
+        ddf = dd.from_pandas(df, npartitions=2)
 
     c.create_table("test", ddf)
     partitioned_ddf = c.sql(
