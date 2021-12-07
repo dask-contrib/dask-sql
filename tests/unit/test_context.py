ff487951382a3fb93069cbfc3eafe5a565e28cde
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2021-12-07 06:28:56 -0800

    Updates tests to use new error type

--- tests/unit/test_context.py
+++ tests/unit/test_context.py
@@ -319,7 +319,7 @@ def test_alter_schema(c):
     c.sql("ALTER SCHEMA test_schema RENAME TO prod_schema")
     assert "prod_schema" in c.schema
 
-    with pytest.raises(RuntimeError):
+    with pytest.raises(KeyError):
         c.sql("ALTER SCHEMA MARVEL RENAME TO DC")
 
     del c.schema["prod_schema"]
@@ -330,7 +330,7 @@ def test_alter_table(c, df_simple):
     c.sql("ALTER TABLE maths RENAME TO physics")
     assert "physics" in c.schema[c.schema_name].tables
 
-    with pytest.raises(RuntimeError):
+    with pytest.raises(KeyError):
         c.sql("ALTER TABLE four_legs RENAME TO two_legs")
 
     c.sql("ALTER TABLE IF EXISTS alien RENAME TO humans")
