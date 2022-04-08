648b448919792abba1fdfd878024155ff0ac6926
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2022-04-08 08:40:11 -0700

    Revert "Disable SQL server functionality (#448)"
    This reverts commit 37a3a61fb13b0c56fcc10bf8ef01f4885a58dae8.

--- dask_sql/__init__.py
+++ dask_sql/__init__.py
@@ -3,12 +3,9 @@ from ._version import get_versions
 from .cmd import cmd_loop
 from .context import Context
 from .datacontainer import Statistics
-
-# from .server.app import run_server
+from .server.app import run_server
 
 __version__ = get_versions()["version"]
 del get_versions
 
-# TODO: re-enable server once CVEs are resolved
-# __all__ = [__version__, cmd_loop, Context, run_server, Statistics]
-__all__ = [__version__, cmd_loop, Context, Statistics]
+__all__ = [__version__, cmd_loop, Context, run_server, Statistics]
