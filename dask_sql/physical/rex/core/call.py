fb29855bea5940c99c28ac634e4754ceced744a5
Jeremy Dyer <jdye64@gmail.com>
2022-05-12 17:21:05 -0400

    When no ELSE statement is present in CASE/WHEN statement default to None

--- dask_sql/physical/rex/core/call.py
+++ dask_sql/physical/rex/core/call.py
@@ -191,8 +191,12 @@ class CaseOperation(Operation):
         where = operands[0]
         then = operands[1]
 
+        print(f"OPERANDS: {operands}")
         if len(operands) > 3:
             other = self.case(*operands[2:])
+        elif len(operands) == 2:
+            # CASE/WHEN statement without an ELSE
+            other = None
         else:
             other = operands[2]
 
