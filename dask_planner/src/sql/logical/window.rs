84bab46ca7f6b6334fc0560ad08b013677c5f174
Ayush Dattagupta <ayushdg95@gmail.com>
2022-05-24 01:40:30 -0700

    Planner: Add bindings for windowFrame and windowFrameBounds

--- dask_planner/src/sql/logical/window.rs
+++ dask_planner/src/sql/logical/window.rs
@@ -1,12 +1,49 @@
 use crate::expression::PyExpr;
 use crate::sql::exceptions::py_type_err;
-use datafusion::logical_expr::{logical_plan::Window, Expr, LogicalPlan};
+use datafusion::logical_expr::{
+    logical_plan::Window, Expr, LogicalPlan, WindowFrame, WindowFrameBound,
+};
 use pyo3::prelude::*;
 
 #[pyclass(name = "Window", module = "dask_planner", subclass)]
 #[derive(Clone)]
 pub struct PyWindow {
     window: Window,
+}
+
+#[pyclass(name = "WindowFrame", module = "dask_planner", subclass)]
+#[derive(Clone)]
+pub struct PyWindowFrame {
+    window_frame: WindowFrame,
+}
+
+#[pyclass(name = "WindowFrameBound", module = "dask_planner", subclass)]
+#[derive(Clone)]
+pub struct PyWindowFrameBound {
+    frame_bound: WindowFrameBound,
+}
+
+impl TryFrom<LogicalPlan> for PyWindow {
+    type Error = PyErr;
+
+    fn try_from(logical_plan: LogicalPlan) -> Result<Self, Self::Error> {
+        match logical_plan {
+            LogicalPlan::Window(window) => Ok(PyWindow { window }),
+            _ => Err(py_type_err("unexpected plan")),
+        }
+    }
+}
+
+impl From<WindowFrame> for PyWindowFrame {
+    fn from(window_frame: WindowFrame) -> Self {
+        PyWindowFrame { window_frame }
+    }
+}
+
+impl From<WindowFrameBound> for PyWindowFrameBound {
+    fn from(frame_bound: WindowFrameBound) -> Self {
+        PyWindowFrameBound { frame_bound }
+    }
 }
 
 #[pymethods]
@@ -84,15 +121,92 @@ impl PyWindow {
             ))),
         }
     }
+    /// Returns a Pywindow frame for a given windowFunction Expression
+    #[pyo3(name = "getWindowFrame")]
+    pub fn get_window_frame(&self, expr: PyExpr) -> Option<PyWindowFrame> {
+        match expr.expr {
+            Expr::WindowFunction { window_frame, .. } => match window_frame {
+                Some(window_frame) => Some(window_frame.clone().into()),
+                None => None,
+            },
+            _ => None,
+        }
+    }
 }
 
-impl TryFrom<LogicalPlan> for PyWindow {
-    type Error = PyErr;
+#[pymethods]
+impl PyWindowFrame {
+    /// Returns window expressions
+    #[pyo3(name = "getFrameUnit")]
+    pub fn get_frame_units(&self) -> PyResult<String> {
+        Ok(self.window_frame.units.to_string())
+    }
+    /// Returns starting bound
+    #[pyo3(name = "getLowerBound")]
+    pub fn get_lower_bound(&self) -> PyResult<PyWindowFrameBound> {
+        Ok(self.window_frame.start_bound.clone().into())
+    }
+    /// Returns end bound
+    #[pyo3(name = "getUpperBound")]
+    pub fn get_upper_bound(&self) -> PyResult<PyWindowFrameBound> {
+        Ok(self.window_frame.end_bound.clone().into())
+    }
+}
 
-    fn try_from(logical_plan: LogicalPlan) -> Result<Self, Self::Error> {
-        match logical_plan {
-            LogicalPlan::Window(window) => Ok(PyWindow { window }),
-            _ => Err(py_type_err("unexpected plan")),
+#[pymethods]
+impl PyWindowFrameBound {
+    /// Returns if the frame bound is current row
+    #[pyo3(name = "isCurrentRow")]
+    pub fn is_current_row(&self) -> bool {
+        match self.frame_bound {
+            WindowFrameBound::CurrentRow => true,
+            _ => false,
+        }
+    }
+    /// Returns if the frame bound is preceding
+    #[pyo3(name = "isPreceding")]
+    pub fn is_preceding(&self) -> bool {
+        match self.frame_bound {
+            WindowFrameBound::Preceding(..) => true,
+            _ => false,
+        }
+    }
+    /// Returns if the frame bound is preceding
+    #[pyo3(name = "isFollowing")]
+    pub fn is_following(&self) -> bool {
+        match self.frame_bound {
+            WindowFrameBound::Following(..) => true,
+            _ => false,
+        }
+    }
+    /// Returns the offset of the window frame
+    #[pyo3(name = "getOffset")]
+    pub fn get_offset(&self) -> PyResult<u64> {
+        match self.frame_bound {
+            WindowFrameBound::Preceding(val) => match val {
+                Some(val) => Ok(val),
+                None => Ok(0),
+            },
+            WindowFrameBound::CurrentRow => Ok(0),
+            WindowFrameBound::Following(val) => match val {
+                Some(val) => Ok(val),
+                None => Ok(0),
+            },
+        }
+    }
+    /// Returns if the frame bound is preceding
+    #[pyo3(name = "isUnbounded")]
+    pub fn is_unbounded(&self) -> bool {
+        match self.frame_bound {
+            WindowFrameBound::Preceding(val) => match val {
+                Some(_) => false,
+                None => true,
+            },
+            WindowFrameBound::CurrentRow => false,
+            WindowFrameBound::Following(val) => match val {
+                Some(_) => false,
+                None => true,
+            },
         }
     }
 }
