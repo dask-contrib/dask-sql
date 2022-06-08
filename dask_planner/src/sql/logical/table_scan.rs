3d967a4d4d565619876b76b515440d32a5886f80
Jeremy Dyer <jdye64@gmail.com>
2022-06-08 12:08:15 -0400

    [REVIEW] - Use iterator instead of for loop and simplify contains_projections

--- dask_planner/src/sql/logical/table_scan.rs
+++ dask_planner/src/sql/logical/table_scan.rs
@@ -14,31 +14,23 @@ pub struct PyTableScan {
 impl PyTableScan {
     #[pyo3(name = "getTableScanProjects")]
     fn scan_projects(&mut self) -> PyResult<Vec<String>> {
-        let mut projs: Vec<String> = Vec::new();
         match &self.table_scan.projection {
-            Some(e) => {
-                for index in e {
-                    projs.push(
-                        (&*self.table_scan.source.schema())
-                            .field(*index)
-                            .name()
-                            .clone(),
-                    );
-                }
+            Some(indices) => {
+                let schema = self.table_scan.source.schema();
+                Ok(indices
+                    .iter()
+                    .map(|i| schema.field(*i).name().to_string())
+                    .collect())
             }
-            None => (),
+            None => Ok(vec![]),
         }
-        Ok(projs)
     }
 
     /// If the 'TableScan' contains columns that should be projected during the
     /// read return True, otherwise return False
     #[pyo3(name = "containsProjections")]
     fn contains_projections(&self) -> bool {
-        match &self.table_scan.projection {
-            Some(_) => true,
-            None => false,
-        }
+        self.table_scan.projection.is_some()
     }
 }
 
