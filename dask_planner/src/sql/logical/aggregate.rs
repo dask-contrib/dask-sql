47efa685662aab6224899edea95689545884a792
Jeremy Dyer <jdye64@gmail.com>
2022-07-20 11:01:37 -0400

    Datafusion version bump (#628)
    * Update DataFusion version, bump
    
    * Added Distinct plugin
    
    * stash updates
    
    * stash update
    
    * All plugins that only need a single name registered changed back to str from List[str]
    
    * InputUtils only needs to pass a single str name value
    
    * uncomment tests
    
    * uncomment tests
    
    * loads of refactoring, isDistinct, get distinct columns function, modeify aggregate.py
    
    * code clean, remove unused import and commented out functions
    
    * Refactor aggreaget.py by moving column names logic for distinct, refactor un-needed logic around schema field names in getDistinctColumns()

--- dask_planner/src/sql/logical/aggregate.rs
+++ dask_planner/src/sql/logical/aggregate.rs
@@ -1,7 +1,6 @@
 use crate::expression::{py_expr_list, PyExpr};
-use std::sync::Arc;
 
-use datafusion_expr::{logical_plan::Aggregate, Expr, LogicalPlan};
+use datafusion_expr::{logical_plan::Aggregate, logical_plan::Distinct, Expr, LogicalPlan};
 
 use crate::sql::exceptions::py_type_err;
 use pyo3::prelude::*;
@@ -9,20 +8,36 @@ use pyo3::prelude::*;
 #[pyclass(name = "Aggregate", module = "dask_planner", subclass)]
 #[derive(Clone)]
 pub struct PyAggregate {
-    aggregate: Aggregate,
+    aggregate: Option<Aggregate>,
+    distinct: Option<Distinct>,
 }
 
 #[pymethods]
 impl PyAggregate {
+    /// Determine the PyExprs that should be "Distinct-ed"
+    #[pyo3(name = "getDistinctColumns")]
+    pub fn distinct_columns(&self) -> PyResult<Vec<String>> {
+        match &self.distinct {
+            Some(e) => Ok(e.input.schema().field_names()),
+            None => panic!("distinct_columns invoked for non distinct instance"),
+        }
+    }
+
     /// Returns a Vec of the group expressions
     #[pyo3(name = "getGroupSets")]
     pub fn group_expressions(&self) -> PyResult<Vec<PyExpr>> {
-        py_expr_list(&self.aggregate.input, &self.aggregate.group_expr)
+        match &self.aggregate {
+            Some(e) => py_expr_list(&e.input, &e.group_expr),
+            None => Ok(vec![]),
+        }
     }
 
     #[pyo3(name = "getNamedAggCalls")]
     pub fn agg_expressions(&self) -> PyResult<Vec<PyExpr>> {
-        py_expr_list(&self.aggregate.input, &self.aggregate.aggr_expr)
+        match &self.aggregate {
+            Some(e) => py_expr_list(&e.input, &e.aggr_expr),
+            None => Ok(vec![]),
+        }
     }
 
     #[pyo3(name = "getAggregationFuncName")]
@@ -36,22 +51,19 @@ impl PyAggregate {
     #[pyo3(name = "getArgs")]
     pub fn aggregation_arguments(&self, expr: PyExpr) -> PyResult<Vec<PyExpr>> {
         match expr.expr {
-            Expr::AggregateFunction { fun: _, args, .. } => {
-                py_expr_list(&self.aggregate.input, &args)
-            }
+            Expr::AggregateFunction { fun: _, args, .. } => match &self.aggregate {
+                Some(e) => py_expr_list(&e.input, &args),
+                None => Ok(vec![]),
+            },
             _ => panic!("Encountered a non Aggregate type in agg_func_name"),
         }
     }
 
     #[pyo3(name = "isDistinct")]
-    pub fn distinct(&self, expr: PyExpr) -> PyResult<bool> {
-        Ok(match expr.expr {
-            Expr::AggregateFunction {
-                fun: _,
-                args: _,
-                distinct,
-            } => distinct,
-            _ => panic!("Encountered a non Aggregate type in agg_func_name"),
+    pub fn distinct(&self) -> PyResult<bool> {
+        Ok(match self.distinct {
+            Some(_) => true,
+            None => false,
         })
     }
 }
@@ -61,7 +73,14 @@ impl TryFrom<LogicalPlan> for PyAggregate {
 
     fn try_from(logical_plan: LogicalPlan) -> Result<Self, Self::Error> {
         match logical_plan {
-            LogicalPlan::Aggregate(aggregate) => Ok(PyAggregate { aggregate }),
+            LogicalPlan::Aggregate(aggregate) => Ok(PyAggregate {
+                aggregate: Some(aggregate),
+                distinct: None,
+            }),
+            LogicalPlan::Distinct(distinct) => Ok(PyAggregate {
+                aggregate: None,
+                distinct: Some(distinct),
+            }),
             _ => Err(py_type_err("unexpected plan")),
         }
     }
