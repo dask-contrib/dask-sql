d366359f7fe7e73c6dd5b56eeb2fd038defe86c6
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2022-07-28 11:44:52 -0400

    Attempt to pull in query files

--- continuous_integration/gpuci/build.sh
+++ continuous_integration/gpuci/build.sh
@@ -41,8 +41,10 @@ gpuci_logger "Install awscli"
 gpuci_mamba_retry install -y -c conda-forge awscli
 
 gpuci_logger "Download TPC-DS dataset"
-gpuci_retry aws s3 cp --only-show-errors s3://rapidsai-data/tpcx-bb-data/tpc-ds/sf1/parquet_2gb/ ./tpc-ds/ --recursive
-ls ./tpc-ds/
+gpuci_retry aws s3 cp --only-show-errors s3://rapidsai-data/tpcx-bb-data/tpc-ds/sf1/parquet_2gb/ ./data/sf1/parquet_2gb/ --recursive
+
+gpuci_logger "Pull in query files"
+git clone git@github.com:rapidsai/tpc-ds.git
 
 gpuci_logger "Install dask"
 python -m pip install git+https://github.com/dask/dask
