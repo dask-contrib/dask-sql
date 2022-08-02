f948429a913c01a87e7589b7d6a59b33134a76da
Ayush Dattagupta <ayushdg95@gmail.com>
2022-08-02 15:38:21 -0700

    Port window logic to datafusion (#545)
    * Planner: Initial window plan bindings
    
    * Move sortexpr methods to expressions
    
    * Update sort expr method calls
    
    * Planner: Add more windowFunction bindings
    
    * Python: Update gby,sort,agg functions calls to planner
    
    * Planner: Add bindings for windowFrame and windowFrameBounds
    
    * Planner: Update window frame offset logic
    
    * Python: Update window physical plan to use new df api
    
    * Python: Update column handling in window physical plan and add tests
    
    * Add column order fixes to window
    
    * re-enable window tests
    
    * Better deafult window bounds, empty windows, add avg operator
    
    * Re-add avg tests, add more count tests
    
    * Python: fix window bounds for 0 offset
    
    * Clean up rust bindings
    
    * Python: Cleanup window function code
    
    * Fix apply_window args removed during cleanup
    
    * Remove serde_json dependecy
    
    * Rust window binding cleanup
    
    * Fix conditional and rust docstrings
    
    * Pass sort attributes without cloning, update bound_description
    
    * Python: Add test comment reason, simplify operator name parsing

--- dask_planner/src/sql/logical/sort.rs
+++ dask_planner/src/sql/logical/sort.rs
@@ -1,7 +1,7 @@
 use crate::expression::{py_expr_list, PyExpr};
 
 use crate::sql::exceptions::py_type_err;
-use datafusion_expr::{logical_plan::Sort, Expr, LogicalPlan};
+use datafusion_expr::{logical_plan::Sort, LogicalPlan};
 use pyo3::prelude::*;
 
 #[pyclass(name = "Sort", module = "dask_planner", subclass)]
@@ -10,51 +10,12 @@ pub struct PySort {
     sort: Sort,
 }
 
-impl PySort {
-    /// Returns if a sort expressions denotes an ascending sort
-    fn is_ascending(&self, expr: &Expr) -> Result<bool, PyErr> {
-        match expr {
-            Expr::Sort { asc, .. } => Ok(*asc),
-            _ => Err(PyErr::new::<pyo3::exceptions::PyTypeError, _>(format!(
-                "Provided Expr {:?} is not a sort type",
-                expr
-            ))),
-        }
-    }
-    /// Returns if nulls should be placed first in a sort expression
-    fn is_nulls_first(&self, expr: &Expr) -> Result<bool, PyErr> {
-        match &expr {
-            Expr::Sort { nulls_first, .. } => Ok(*nulls_first),
-            _ => Err(PyErr::new::<pyo3::exceptions::PyTypeError, _>(format!(
-                "Provided Expr {:?} is not a sort type",
-                expr
-            ))),
-        }
-    }
-}
 #[pymethods]
 impl PySort {
     /// Returns a Vec of the sort expressions
     #[pyo3(name = "getCollation")]
     pub fn sort_expressions(&self) -> PyResult<Vec<PyExpr>> {
         py_expr_list(&self.sort.input, &self.sort.expr)
-    }
-
-    #[pyo3(name = "getAscending")]
-    pub fn get_ascending(&self) -> PyResult<Vec<bool>> {
-        self.sort
-            .expr
-            .iter()
-            .map(|sortexpr| self.is_ascending(sortexpr))
-            .collect::<Result<Vec<_>, _>>()
-    }
-    #[pyo3(name = "getNullsFirst")]
-    pub fn get_nulls_first(&self) -> PyResult<Vec<bool>> {
-        self.sort
-            .expr
-            .iter()
-            .map(|sortexpr| self.is_nulls_first(sortexpr))
-            .collect::<Result<Vec<_>, _>>()
     }
 }
 
