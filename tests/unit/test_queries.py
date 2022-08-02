f1c5fc52494a8aa58734bc22268da9c939d2ec72
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2022-08-02 10:26:08 -0700

    xfail failing queries

--- tests/unit/test_queries.py
+++ tests/unit/test_queries.py
@@ -2,8 +2,90 @@ import os
 
 import pytest
 
+XFAIL_QUERIES = (
+    1,
+    2,
+    4,
+    5,
+    6,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    16,
+    17,
+    18,
+    20,
+    21,
+    22,
+    23,
+    24,
+    27,
+    28,
+    30,
+    32,
+    34,
+    35,
+    36,
+    37,
+    39,
+    40,
+    41,
+    43,
+    44,
+    45,
+    47,
+    48,
+    49,
+    50,
+    51,
+    53,
+    54,
+    57,
+    58,
+    59,
+    62,
+    63,
+    64,
+    66,
+    67,
+    69,
+    70,
+    72,
+    73,
+    74,
+    75,
+    76,
+    77,
+    78,
+    80,
+    81,
+    82,
+    83,
+    84,
+    85,
+    86,
+    87,
+    88,
+    89,
+    92,
+    94,
+    95,
+    97,
+    98,
+    99,
+)
+
 QUERIES = [
-    f"q{i}a.sql" if i in {14, 23, 24, 39} else f"q{i}.sql" for i in range(1, 100)
+    pytest.param(f"q{i}a.sql", marks=pytest.mark.xfail if i in XFAIL_QUERIES else None)
+    if i in {14, 23, 24, 39}
+    else pytest.param(
+        f"q{i}.sql", marks=pytest.mark.xfail if i in XFAIL_QUERIES else None
+    )
+    for i in range(1, 100)
 ]
 
 
