342ec488a660c633bc1112a574219710ae525199
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2022-08-03 11:22:43 -0700

    Move shared logic between CTAS plugin and Context.sql to _compute_table_from_rel

--- dask_sql/physical/rel/custom/create_table_as.py
+++ dask_sql/physical/rel/custom/create_table_as.py
@@ -1,10 +1,8 @@
 import logging
-from collections import Counter
 from typing import TYPE_CHECKING
 
 from dask_sql.datacontainer import DataContainer
 from dask_sql.physical.rel.base import BaseRelPlugin
-from dask_sql.physical.rel.convert import RelConverter
 
 if TYPE_CHECKING:
     import dask_sql
@@ -57,29 +55,8 @@ class CreateTableAsPlugin(BaseRelPlugin):
             f"Creating new table with name {table_name} and logical plan {input_rel}"
         )
 
-        dc = RelConverter.convert(input_rel, context=context)
-        select_names = [field for field in input_rel.getRowType().getFieldList()]
-
-        if select_names:
-            # Use FQ name if not unique and simple name if it is unique. If a join contains the same column
-            # names the output col is prepended with the fully qualified column name
-            field_counts = Counter([field.getName() for field in select_names])
-            select_names = [
-                field.getQualifiedName()
-                if field_counts[field.getName()] > 1
-                else field.getName()
-                for field in select_names
-            ]
-
-            cc = dc.column_container
-            cc = cc.rename(
-                {
-                    df_col: select_name
-                    for df_col, select_name in zip(cc.columns, select_names)
-                }
-            )
-            dc = DataContainer(dc.df, cc)
-
-        df = dc.assign()
-
-        context.create_table(table_name, df, schema_name=schema_name)
+        context.create_table(
+            table_name,
+            context._compute_table_from_rel(input_rel),
+            schema_name=schema_name,
+        )
