f4b99ad2c2971376b5f2e97f7ece0f300315c8a1
Jeremy Dyer <jdye64@gmail.com>
2022-08-24 14:47:15 -0400

    Add logic for PREDICT model parsing

--- dask_planner/src/sql/optimizer.rs
+++ dask_planner/src/sql/optimizer.rs
@@ -7,7 +7,7 @@ use datafusion_optimizer::{
     common_subexpr_eliminate::CommonSubexprEliminate, eliminate_limit::EliminateLimit,
     filter_null_join_keys::FilterNullJoinKeys, filter_push_down::FilterPushDown,
     limit_push_down::LimitPushDown, optimizer::OptimizerRule,
-    projection_push_down::ProjectionPushDown, single_distinct_to_groupby::SingleDistinctToGroupBy,
+    projection_push_down::ProjectionPushDown,
     subquery_filter_to_join::SubqueryFilterToJoin, OptimizerConfig,
 };
 
