59d69160811de1817187b8c13d88d5f96b5283a0
Andy Grove <andygrove73@gmail.com>
2022-09-30 10:59:09 -0600

    revert change

--- tests/integration/test_rex.py
+++ tests/integration/test_rex.py
@@ -73,7 +73,7 @@ def test_literals(c):
         """SELECT 'a string äö' AS "S",
                     4.4 AS "F",
                     -4564347464 AS "I",
-                    TIME '08:08:00.091' AS "T",
+                    -- TIME '08:08:00.091' AS "T",
                     TIMESTAMP '2022-04-06 17:33:21' AS "DT",
                     DATE '1991-06-02' AS "D",
                     INTERVAL '1' DAY AS "IN"
@@ -85,7 +85,7 @@ def test_literals(c):
             "S": ["a string äö"],
             "F": [4.4],
             "I": [-4564347464],
-            "T": [pd.to_datetime("1970-01-01 08:08:00.091")],
+            # "T": [pd.to_datetime("1970-01-01 08:08:00.091")], Depends on https://github.com/apache/arrow-datafusion/issues/2883"
             "DT": [pd.to_datetime("2022-04-06 17:33:21")],
             "D": [pd.to_datetime("1991-06-02 00:00")],
             "IN": [pd.to_timedelta("1d")],
