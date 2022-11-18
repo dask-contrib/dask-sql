9a9fa1ed7a78162b3dcc019cbe3bc6cef59b93ff
Ayush Dattagupta <ayushdg95@gmail.com>
2022-11-18 11:38:58 -0800

    Update PyTableScan to include input schema

--- dask_planner/src/sql/logical/table_scan.rs
+++ dask_planner/src/sql/logical/table_scan.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use datafusion_common::DFSchema;
 use datafusion_expr::{logical_plan::TableScan, LogicalPlan};
 use pyo3::prelude::*;
 
@@ -12,6 +13,7 @@ use crate::{
 #[derive(Clone)]
 pub struct PyTableScan {
     pub(crate) table_scan: TableScan,
+    input: Arc<LogicalPlan>,
 }
 
 #[pymethods]
@@ -39,10 +41,7 @@ impl PyTableScan {
 
     #[pyo3(name = "getFilters")]
     fn scan_filters(&self) -> PyResult<Vec<PyExpr>> {
-        py_expr_list(
-            &Arc::new(LogicalPlan::TableScan(self.table_scan.clone())),
-            &self.table_scan.filters,
-        )
+        py_expr_list(&self.input, &self.table_scan.filters)
     }
 }
 
@@ -51,7 +50,20 @@ impl TryFrom<LogicalPlan> for PyTableScan {
 
     fn try_from(logical_plan: LogicalPlan) -> Result<Self, Self::Error> {
         match logical_plan {
-            LogicalPlan::TableScan(table_scan) => Ok(PyTableScan { table_scan }),
+            LogicalPlan::TableScan(table_scan) => {
+                // Create an input logical plan that's identical to the table scan with schema from the table source
+                let mut input = table_scan.clone();
+                input.projected_schema = DFSchema::try_from_qualified_schema(
+                    &table_scan.table_name,
+                    &table_scan.source.schema(),
+                )
+                .map_or(input.projected_schema, Arc::new);
+
+                Ok(PyTableScan {
+                    table_scan,
+                    input: Arc::new(LogicalPlan::TableScan(input)),
+                })
+            }
             _ => Err(py_type_err("unexpected plan")),
         }
     }
