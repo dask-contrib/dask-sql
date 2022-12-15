d3eb7fca021077788f08d8dd5365b930e8459c8e
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2022-12-15 10:27:54 -0800

    Assert that old table/schema name is gone

--- tests/unit/test_context.py
+++ tests/unit/test_context.py
@@ -395,6 +395,7 @@ def test_alter_schema(c):
     c.create_schema("test_schema")
     c.sql("ALTER SCHEMA test_schema RENAME TO prod_schema")
     assert "prod_schema" in c.schema
+    assert "test_schema" not in c.schema
 
     with pytest.raises(KeyError):
         c.sql("ALTER SCHEMA MARVEL RENAME TO DC")
@@ -406,6 +407,7 @@ def test_alter_table(c, df_simple):
     c.create_table("maths", df_simple)
     c.sql("ALTER TABLE maths RENAME TO physics")
     assert "physics" in c.schema[c.schema_name].tables
+    assert "maths" not in c.schema[c.schema_name].tables
 
     with pytest.raises(KeyError):
         c.sql("ALTER TABLE four_legs RENAME TO two_legs")
