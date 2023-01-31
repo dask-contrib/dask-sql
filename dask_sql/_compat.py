3c89e1aabf66181c50957c368e3cb7e863d25aeb
Ayush Dattagupta <ayushdg95@gmail.com>
2023-01-31 13:11:52 -0800

    Update bcast join dask version comparision [test-upstream]

--- dask_sql/_compat.py
+++ dask_sql/_compat.py
@@ -14,4 +14,4 @@ INDEXER_WINDOW_STEP_IMPLEMENTED = _pandas_version >= parseVersion("1.5.0")
 # TODO: remove if prompt-toolkit min version gets bumped
 PIPE_INPUT_CONTEXT_MANAGER = _prompt_toolkit_version >= parseVersion("3.0.29")
 
-BROADCAST_JOIN_SUPPORT_WORKING = _dask_version >= parseVersion("2023.1.1")
+BROADCAST_JOIN_SUPPORT_WORKING = _dask_version > parseVersion("2023.1.0")
