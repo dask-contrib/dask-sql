aaa0c576507573d3d2523830ac50736e8acf7da1
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2023-05-30 20:35:33 -0700

    Attempt to use cached work directoryls

--- continuous_integration/recipe/build.sh
+++ continuous_integration/recipe/build.sh
@@ -2,7 +2,12 @@
 
 export RUST_BACKTRACE=full
 
-# pull from existing rust cache, if it exists
+# pull cached work directory if we are in a github workflow
+# if [[ -v GITHUB_WORKSPACE && -d "$GITHUB_WORKSPACE/dask_planner/target" ]]; then
+#     ln -s $GITHUB_WORKSPACE/dask_planner/target $CONDA_PREFIX/../work/dask_planner/target
+# if
+
+# pull cached registry from existing rust installation, if any
 if [ -d "$HOME/.cargo/registry" ]; then ln -s $HOME/.cargo/registry $CONDA_PREFIX/.cargo/registry; fi
 if [ -d "$HOME/.cargo/git" ]; then ln -s $HOME/.cargo/git $CONDA_PREFIX/.cargo/git; fi
 
