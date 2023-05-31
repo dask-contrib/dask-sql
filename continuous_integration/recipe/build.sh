fb80b3174856d0bb4939eba46e1da0574f3742d7
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2023-05-31 08:02:11 -0700

    First pass at caching intermediate compilation work

--- continuous_integration/recipe/build.sh
+++ continuous_integration/recipe/build.sh
@@ -2,10 +2,13 @@
 
 export RUST_BACKTRACE=full
 
-# pull cached work directory if we are in a github workflow
-# if [[ -v GITHUB_WORKSPACE && -d "$GITHUB_WORKSPACE/dask_planner/target" ]]; then
-#     ln -s $GITHUB_WORKSPACE/dask_planner/target $CONDA_PREFIX/../work/dask_planner/target
-# if
+# set up caching if we are being executed in a github workflow
+if [[ -v GITHUB_WORKSPACE ]]; then
+    if [[ ! -d "$GITHUB_WORKSPACE/dask_planner/target" ]]; then
+        mkdir "$GITHUB_WORKSPACE/dask_planner/target"
+    fi
+    ln -s $GITHUB_WORKSPACE/dask_planner/target $CONDA_PREFIX/../work/dask_planner/target
+fi
 
 # pull cached registry from existing rust installation, if any
 if [ -d "$HOME/.cargo/registry" ]; then ln -s $HOME/.cargo/registry $CONDA_PREFIX/.cargo/registry; fi
