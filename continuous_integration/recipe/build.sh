6d6841b2e2e1acafbdb6d11da71fedc96b64dff0
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2023-05-30 19:20:01 -0700

    Fix typo

--- continuous_integration/recipe/build.sh
+++ continuous_integration/recipe/build.sh
@@ -3,11 +3,7 @@
 export RUST_BACKTRACE=full
 
 # pull from existing rust cache, if it exists
-if [ -d "~/.cargo/registry" ]; then
-    ln -s ~/.cargo/registry $CONDA_PREFIX/.cargo/registry
-else
-if [ -d "~/.cargo/git" ]; then
-    ln -s ~/.cargo/git $CONDA_PREFIX/.cargo/git
-else
+if [ -d "~/.cargo/registry" ]; then ln -s ~/.cargo/registry $CONDA_PREFIX/.cargo/registry; fi
+if [ -d "~/.cargo/git" ]; then ln -s ~/.cargo/git $CONDA_PREFIX/.cargo/git; fi
 
 $PYTHON -m pip install . --no-deps -vv
