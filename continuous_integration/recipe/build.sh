9ff5b0604f75944ac0adcb9d8399ad7e3e5132df
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2023-05-30 19:37:26 -0700

    Use proper home variable for build script

--- continuous_integration/recipe/build.sh
+++ continuous_integration/recipe/build.sh
@@ -3,7 +3,7 @@
 export RUST_BACKTRACE=full
 
 # pull from existing rust cache, if it exists
-if [ -d "~/.cargo/registry" ]; then ln -s ~/.cargo/registry $CONDA_PREFIX/.cargo/registry; fi
-if [ -d "~/.cargo/git" ]; then ln -s ~/.cargo/git $CONDA_PREFIX/.cargo/git; fi
+if [ -d "$HOME/.cargo/registry" ]; then ln -s $HOME/.cargo/registry $CONDA_PREFIX/.cargo/registry; fi
+if [ -d "$HOME/.cargo/git" ]; then ln -s $HOME/.cargo/git $CONDA_PREFIX/.cargo/git; fi
 
 $PYTHON -m pip install . --no-deps -vv
