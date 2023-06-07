51cd0c8a3ae6a9ce5184b62254b073a2adbaf739
Richard (Rick) Zamora <rzamora217@gmail.com>
2023-06-07 09:35:32 -0500

    Support predicate-pushdown for `isin` operations (#1160)
    * add basic isin workaround
    
    * add  case - but blocked by upstream-dask bug (I think)
    
    * link dask PR
    
    * update test to skip for released dask versions
    
    * add isna support
    
    * use from_pandas instead of from_dict
    
    * use from_pandas instead of from_dict (second attempt)
    
    * add more dask-version checks
    
    * use None instead of np.nan
    
    * bail if existing filters, and move version check
    
    * remove check for pre-existing filters (CI experiment)
    
    * revert last commit
    
    * minor _blockwise_comparison_dnf fix
    
    * drop non-tuples
    
    * bail for logical operation containing literal boolean operand
    
    * bail on TypeError in addition to ValueError
    
    * add tests to test_utils

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import pytest
 from dask import dataframe as dd
+from dask.utils_test import hlg_layer
 
+from dask_sql._compat import PQ_IS_SUPPORT, PQ_NOT_IN_SUPPORT
+from dask_sql.physical.utils.filter import attempt_predicate_pushdown
 from dask_sql.utils import Pluggable, is_frame
 
 
@@ -52,3 +55,78 @@ def test_overwrite():
 
     assert PluginTest1.get_plugin("some_key") == "value_2"
     assert PluginTest1().get_plugin("some_key") == "value_2"
+
+
+def test_predicate_pushdown_simple(parquet_ddf):
+    filtered_df = parquet_ddf[parquet_ddf["a"] > 1]
+    pushdown_df = attempt_predicate_pushdown(filtered_df)
+    got_filters = hlg_layer(pushdown_df.dask, "read-parquet").creation_info["kwargs"][
+        "filters"
+    ]
+    got_filters = frozenset(frozenset(v) for v in got_filters)
+    expected_filters = [[("a", ">", 1)]]
+    expected_filters = frozenset(frozenset(v) for v in expected_filters)
+    assert got_filters == expected_filters
+
+
+def test_predicate_pushdown_logical(parquet_ddf):
+    filtered_df = parquet_ddf[
+        (parquet_ddf["a"] > 1) & (parquet_ddf["b"] < 2) | (parquet_ddf["a"] == -1)
+    ]
+
+    pushdown_df = attempt_predicate_pushdown(filtered_df)
+    got_filters = hlg_layer(pushdown_df.dask, "read-parquet").creation_info["kwargs"][
+        "filters"
+    ]
+    got_filters = frozenset(frozenset(v) for v in got_filters)
+    expected_filters = [[("a", ">", 1), ("b", "<", 2)], [("a", "==", -1)]]
+    expected_filters = frozenset(frozenset(v) for v in expected_filters)
+    assert got_filters == expected_filters
+
+
+@pytest.mark.skipif(
+    not PQ_NOT_IN_SUPPORT,
+    reason="Requires https://github.com/dask/dask/pull/10320",
+)
+def test_predicate_pushdown_in(parquet_ddf):
+    filtered_df = parquet_ddf[
+        (parquet_ddf["a"] > 1) & (parquet_ddf["b"] < 2)
+        | (parquet_ddf["a"] == -1) & parquet_ddf["c"].isin(("A", "B", "C"))
+        | ~parquet_ddf["b"].isin((5, 6, 7))
+    ]
+    pushdown_df = attempt_predicate_pushdown(filtered_df)
+    got_filters = hlg_layer(pushdown_df.dask, "read-parquet").creation_info["kwargs"][
+        "filters"
+    ]
+    got_filters = frozenset(frozenset(v) for v in got_filters)
+    expected_filters = [
+        [("b", "<", 2), ("a", ">", 1)],
+        [("a", "==", -1), ("c", "in", ("A", "B", "C"))],
+        [("b", "not in", (5, 6, 7))],
+    ]
+    expected_filters = frozenset(frozenset(v) for v in expected_filters)
+    assert got_filters == expected_filters
+
+
+@pytest.mark.skipif(
+    not PQ_IS_SUPPORT,
+    reason="Requires dask>=2023.3.1",
+)
+def test_predicate_pushdown_isna(parquet_ddf):
+    filtered_df = parquet_ddf[
+        (parquet_ddf["a"] > 1) & (parquet_ddf["b"] < 2)
+        | (parquet_ddf["a"] == -1) & ~parquet_ddf["c"].isna()
+        | parquet_ddf["b"].isna()
+    ]
+    pushdown_df = attempt_predicate_pushdown(filtered_df)
+    got_filters = hlg_layer(pushdown_df.dask, "read-parquet").creation_info["kwargs"][
+        "filters"
+    ]
+    got_filters = frozenset(frozenset(v) for v in got_filters)
+    expected_filters = [
+        [("b", "<", 2), ("a", ">", 1)],
+        [("a", "==", -1), ("c", "is not", None)],
+        [("b", "is", None)],
+    ]
+    expected_filters = frozenset(frozenset(v) for v in expected_filters)
+    assert got_filters == expected_filters
