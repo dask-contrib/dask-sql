899170679b44b8eebd5efe1af373eded98cbe9aa
Sarah Yurick <53962159+sarahyurick@users.noreply.github.com>
2023-06-20 16:21:49 -0700

    Add command line arguments for testing queries (#1161)
    * add filepath arg
    
    * data_dir and queries_dir
    
    ---------
    
    Co-authored-by: Ayush Dattagupta <ayushdg95@gmail.com>

--- conftest.py
+++ conftest.py
@@ -7,6 +7,8 @@ pytest_plugins = ["tests.integration.fixtures"]
 def pytest_addoption(parser):
     parser.addoption("--rungpu", action="store_true", help="run tests meant for GPU")
     parser.addoption("--runqueries", action="store_true", help="run test queries")
+    parser.addoption("--data_dir", help="specify file path to the data")
+    parser.addoption("--queries_dir", help="specify file path to the queries")
 
 
 def pytest_runtest_setup(item):
@@ -21,3 +23,13 @@ def pytest_runtest_setup(item):
         dask.config.set({"dataframe.shuffle.algorithm": None})
     if "queries" in item.keywords and not item.config.getoption("--runqueries"):
         pytest.skip("need --runqueries option to run")
+
+
+@pytest.fixture(scope="session")
+def data_dir(request):
+    return request.config.getoption("--data_dir")
+
+
+@pytest.fixture(scope="session")
+def queries_dir(request):
+    return request.config.getoption("--queries_dir")
