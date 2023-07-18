68d190c21c456013fe7d767b74729246e06f1e53
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2023-07-18 15:46:04 -0700

    Remove glibc constraint from aarch64 packages

--- dask_sql/__init__.py
+++ dask_sql/__init__.py
@@ -1,3 +1,7 @@
+# load in shared rust object first to minimize risk of aarch64 glibc TLS allocation bug
+# https://bugzilla.redhat.com/show_bug.cgi?id=1722181
+import dask_planner.rust
+
 from . import _version, config
 from .cmd import cmd_loop
 from .context import Context
