995794571b51e1e551408f59082fa0dc1dce82f4
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2023-10-13 12:05:37 -0700

    Use importlib.metadata for version

--- dask_sql/__init__.py
+++ dask_sql/__init__.py
@@ -2,12 +2,14 @@
 # https://github.com/dask-contrib/dask-sql/issues/1169
 from . import _datafusion_lib  # isort:skip
 
+import importlib.metadata
+
 from . import config
 from .cmd import cmd_loop
 from .context import Context
 from .datacontainer import Statistics
 from .server.app import run_server
 
-__version__ = "2023.10.0"
+__version__ = importlib.metadata.version(__name__)
 
 __all__ = [__version__, cmd_loop, Context, run_server, Statistics]
