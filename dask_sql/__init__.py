58448ac903f34db02fcd02387dadd4179d5c115e
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2023-10-13 11:36:28 -0700

    Remove versioneer

--- dask_sql/__init__.py
+++ dask_sql/__init__.py
@@ -2,12 +2,12 @@
 # https://github.com/dask-contrib/dask-sql/issues/1169
 from . import _datafusion_lib  # isort:skip
 
-from . import _version, config
+from . import config
 from .cmd import cmd_loop
 from .context import Context
 from .datacontainer import Statistics
 from .server.app import run_server
 
-__version__ = _version.get_versions()["version"]
+__version__ = "2023.10.0"
 
 __all__ = [__version__, cmd_loop, Context, run_server, Statistics]
