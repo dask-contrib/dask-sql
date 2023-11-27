d19ba84993f07d1fa16e451774bf53d867b86f61
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2023-11-27 10:57:24 -0800

    Linting

--- build.rs
+++ build.rs
@@ -1,12 +1,13 @@
 fn main() {
+    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    println!("cargo:rerun-if-env-changed=TARGET_ARCH");
+    println!("cargo:rerun-if-env-changed=CARGO_CFG_TARGET_OS");
+    println!("cargo:rerun-if-env-changed=CARGO_CFG_TARGET_ARCH");
 
-    match target_arch.as_str() {
-        "aarch64" => {
-            println!("cargo:warning=Building for aarch64, which needs dynamic TLS enabled for mimalloc");
-            println!("cargo:rustc-cfg=NEEDS_DYNAMIC_TLS");
-        }
-        _ => {}
+    if target_os.as_str() == "linux" && target_arch.as_str() == "aarch64" {
+        println!(
+            "cargo:warning=Building for aarch64, which needs dynamic TLS enabled for mimalloc"
+        );
+        println!("cargo:rustc-cfg=NEEDS_DYNAMIC_TLS");
     }
 }
