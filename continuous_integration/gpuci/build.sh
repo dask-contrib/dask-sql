3c24e26a10bc63347802112c334aa58ea9d010f0
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2023-11-28 11:01:02 -0800

    Revert "Resolve deprecation warnings"
    This reverts commit 36ace7a022946007e642d3171df4a2e57560db26.

--- continuous_integration/gpuci/build.sh
+++ continuous_integration/gpuci/build.sh
@@ -27,38 +27,38 @@ export CUDA_REL=${CUDA_VERSION%.*}
 # SETUP - Check environment
 ################################################################################
 
-rapids-logger "Check environment variables"
+gpuci_logger "Check environment variables"
 env
 
-rapids-logger "Check GPU usage"
+gpuci_logger "Check GPU usage"
 nvidia-smi
 
-rapids-logger "Activate conda env"
+gpuci_logger "Activate conda env"
 . /opt/conda/etc/profile.d/conda.sh
 conda activate dask_sql
 
-rapids-logger "Update conda env"
+gpuci_logger "Update conda env"
 gpuci_mamba_retry env update -n dask_sql -f continuous_integration/gpuci/environment-${PYTHON_VER}.yaml
 
-rapids-logger "Install awscli"
+gpuci_logger "Install awscli"
 gpuci_mamba_retry install -y -c conda-forge awscli
 
-rapids-logger "Download parquet dataset"
-rapids-retry aws s3 cp --only-show-errors "${DASK_SQL_BUCKET_NAME}parquet_2gb_sorted/" tests/unit/data/ --recursive
+gpuci_logger "Download parquet dataset"
+gpuci_retry aws s3 cp --only-show-errors "${DASK_SQL_BUCKET_NAME}parquet_2gb_sorted/" tests/unit/data/ --recursive
 
-rapids-logger "Download query files"
-rapids-retry aws s3 cp --only-show-errors "${DASK_SQL_BUCKET_NAME}queries/" tests/unit/queries/ --recursive
+gpuci_logger "Download query files"
+gpuci_retry aws s3 cp --only-show-errors "${DASK_SQL_BUCKET_NAME}queries/" tests/unit/queries/ --recursive
 
-rapids-logger "Install dask-sql"
+gpuci_logger "Install dask-sql"
 pip install -e . -vv
 
-rapids-logger "Check Python version"
+gpuci_logger "Check Python version"
 python --version
 
-rapids-logger "Check conda environment"
+gpuci_logger "Check conda environment"
 conda info
 conda config --show-sources
 conda list --show-channel-urls
 
-rapids-logger "Python py.test for dask-sql"
+gpuci_logger "Python py.test for dask-sql"
 py.test $WORKSPACE -n 4 -v -m gpu --runqueries --rungpu --junitxml="$WORKSPACE/junit-dask-sql.xml" --cov-config="$WORKSPACE/.coveragerc" --cov=dask_sql --cov-report=xml:"$WORKSPACE/dask-sql-coverage.xml" --cov-report term
