fddf52f7df1908f660f15c5130e48939399df059
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2024-04-10 07:33:01 -0700

    Skip over window count tests for now

--- tests/integration/test_compatibility.py
+++ tests/integration/test_compatibility.py
@@ -19,7 +19,7 @@ import pytest
 
 from dask_sql import Context
 from dask_sql.utils import ParsingException
-from tests.utils import assert_eq, convert_nullable_columns
+from tests.utils import assert_eq, convert_nullable_columns, skipif_dask_expr_enabled
 
 
 def eq_sqlite(sql, **dfs):
@@ -813,6 +813,7 @@ def test_window_min_max_partition_by():
         )
 
 
+@skipif_dask_expr_enabled()
 def test_window_count():
     for func in ["COUNT"]:
         a = make_rand_df(100, a=float, b=(int, 50), c=(str, 50))
@@ -863,6 +864,7 @@ def test_window_count():
         )
 
 
+@skipif_dask_expr_enabled()
 def test_window_count_partition_by():
     for func in ["COUNT"]:
         a = make_rand_df(100, a=float, b=(int, 50), c=(str, 50))
