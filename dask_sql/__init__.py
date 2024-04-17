b3862cf812782c1861278d88c53f297f1f900640
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2024-04-17 09:02:51 -0400

    Move Dask config overrides to sql.yaml (#1260)
    * Move dataframe overrides to sql.yaml
    
    * Un-xfail test_xgboost_training_prediction on win32
    
    * Revert "Un-xfail test_xgboost_training_prediction on win32"
    
    This reverts commit 16ca14f846f6d81783f33b383bc9beee87ead423.
    
    * Move overrides to import-time
    
    * Add config overrides back to conftest.py

--- dask_sql/__init__.py
+++ dask_sql/__init__.py
@@ -4,11 +4,16 @@ from . import _datafusion_lib  # isort:skip
 
 import importlib.metadata
 
+from dask.config import set
+
 from . import config
 from .cmd import cmd_loop
 from .context import Context
 from .datacontainer import Statistics
 from .server.app import run_server
+
+# TODO: get pyarrow strings and p2p shuffle working
+set(dataframe__convert_string=False, dataframe__shuffle__method="tasks")
 
 __version__ = importlib.metadata.version(__name__)
 
